79ed4102b70c35f56ad12cea09332b02ad395161
sachinkssachu95 <91598648+sachinkssachu95@users.noreply.github.com>
2021-09-30 16:16:58 +0530

    Add files via upload

---
+++ Script (1).js
@@ -0,0 +1,24 @@
+//navbar
+function hideIconBar()
+{
+    var iconBar= document.getElementById("iconBar");
+    var navigation= document.getElementById("navigation");
+    iconBar.setAttribute("style","display:none;");
+    navigation.classList.remove("hide");
+}
+function showIconBar()
+{
+    var iconBar= document.getElementById("iconBar");
+    var navigation= document.getElementById("navigation");
+    iconBar.setAttribute("style","display:block;");
+    navigation.classList.add("hide");
+}
+//comment
+function showComment(){
+    var commentArea= document.getElementById("comment-area");
+    commentArea.setAttribute("style", "display:block;");
+}
+function showReply(){
+    var replyArea= document.getElementById("reply-area");
+    replyArea.setAttribute("style", "display:block;");
+}
